924653740d5d31ae08cdf9600144bccda0279f7b
finotellicarlos <carlos.finotelli@outlook.com>
2024-02-07 13:24:38 -0300

    feAt: add toasts to create new note

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
+import { toast } from "sonner";
 
 export function NewNoteCard() {
   const [shouldShowOnboarding, setShouldShowOnboarding] =
@@ -21,7 +22,7 @@ export function NewNoteCard() {
   function handleSaveNote(event: FormEvent) {
     event.preventDefault();
 
-    console.log(contentNote);
+    toast.success("Nota criada com sucesso!");
   }
 
   return (
