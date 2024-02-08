3505ecc3319fab35c0ba813b60ca88c6133340ff
finotellicarlos <carlos.finotelli@outlook.com>
2024-02-08 16:11:28 -0300

    feat: create recorgind using api speechRecognition

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,6 +3,8 @@ import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from "sonner";
 
+let speechRecognition: SpeechRecognition | null = null;
+
 export function NewNoteCard({
   onNoteCreated,
 }: {
@@ -42,9 +44,48 @@ export function NewNoteCard({
 
   function handleStartRecording() {
     setIsRecording(true);
+
+    const isSpeechRecognitionAPIAvailable =
+      "SpeechRecognition" in window || "webkitSpeechRecognition" in window;
+
+    if (!isSpeechRecognitionAPIAvailable) {
+      toast.error("Infelizmente seu navegador não suporta a API de gravação!");
+      return;
+    }
+
+    setIsRecording(true);
+    setShouldShowOnboarding(false);
+
+    const SpeechRecognitionAPI =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    speechRecognition = new SpeechRecognitionAPI();
+
+    speechRecognition.lang = "pt-BR";
+    speechRecognition.continuous = true;
+    speechRecognition.maxAlternatives = 1;
+    speechRecognition.interimResults = true;
+
+    speechRecognition.onresult = (event) => {
+      const transcription = Array.from(event.results).reduce((text, result) => {
+        return text.concat(result[0].transcript);
+      }, "");
+
+      setContentNote(transcription);
+    };
+
+    speechRecognition.onerror = (event) => {
+      console.log(event.error);
+    };
+
+    speechRecognition.start();
   }
 
   function handleStopRecording() {
+    if (speechRecognition !== null) {
+      speechRecognition.stop();
+    }
+
     setIsRecording(false);
   }
 
